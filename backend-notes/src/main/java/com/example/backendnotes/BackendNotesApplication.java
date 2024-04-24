8b7b6f95c46dd2a54d3a50b945ff2c79a6c388cd
Emerson <emerson.vv75248@gmail.com>
2024-04-24 18:53:56 -0300

    Backend

---
+++ backend-notes/src/main/java/com/example/backendnotes/BackendNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNotesApplication.class, args);
+	}
+
+}
---
+++ backend-notes/src/test/java/com/example/backendnotes/BackendNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
