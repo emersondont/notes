62fd70e1f1fdc1ca9f2acb159dba8abf40405ed7
Emerson <emerson.vv75248@gmail.com>
2024-05-01 23:59:49 -0300

    add @CrossOrigin

--- backend-notes/src/main/java/com/example/backendnotes/controllers/ListController.java
+++ backend-notes/src/main/java/com/example/backendnotes/controllers/ListController.java
@@ -18,6 +18,7 @@ import java.util.UUID;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@CrossOrigin
 @RestController
 public class ListController {
 
